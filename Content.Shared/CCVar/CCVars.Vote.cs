1e5b5ed42af3f264893204e503523650d1f23ed0
Nathan Douglas <ndouglas1245@gmail.com>
2025-09-30 15:17:41 +0100

    Rule change and new button! as well as event frequency and type changes.
    -added own table for meteorswarms
    -added own gamePreset
    -added own EventTable
    -changed the minimun player count on some maps for them to be voteable with a low player count

--- Content.Shared/CCVar/CCVars.Vote.cs
+++ Content.Shared/CCVar/CCVars.Vote.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.Configuration;
+using Robust.Shared.Configuration;
 
 namespace Content.Shared.CCVar;
 
@@ -50,7 +50,7 @@ public sealed partial class CCVars
     /// Whether or not to prevent the restart vote from having any effect when there is an online admin
     /// </summary>
     public static readonly CVarDef<bool> VoteRestartNotAllowedWhenAdminOnline =
-        CVarDef.Create("vote.restart_not_allowed_when_admin_online", true, CVar.SERVERONLY);
+        CVarDef.Create("vote.restart_not_allowed_when_admin_online", false, CVar.SERVERONLY);
 
     /// <summary>
     ///     The delay which two votes of the same type are allowed to be made by separate people, in seconds.
---
+++ Resources/Locale/en-US/game-ticking/game-presets/preset-huskeshift.ftl
@@ -0,0 +1,2 @@
+huske-title = Huske Shift!
+huske-description = Greenshift, but with mild events! (keeps it somewhat entertaining)
